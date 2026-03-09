a6cd661eb4b5ee1fb80ce560ee6494b021e676ee
victor <victorcalehr18@gmail.com>
2026-03-08 23:44:17 -0500

    top text for login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,19 @@
 import { useState } from "react";
 import { GlobeIcon, MailIcon, LockIcon, EyeIcon, EyeOffIcon, AlertIcon } from "../components/Icons";
 import "../styles/login.css";
+
+export default function LoginPage({ onLogin }) {
+    return (
+        <div className="login-wrap">
+          <div className="login-card">
+            {/* Logo */}
+            <div className="login-logo">
+              <div className="login-logo-icon">
+                <GlobeIcon />
+              </div>
+              <h1>Wanderwise</h1>
+            </div>
+        </div>
+        </div>
+    )
+}
