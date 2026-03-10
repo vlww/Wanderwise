77a3706afd08e2693c9c6f8657727b9e61f0e9d4
victor <victorcalehr18@gmail.com>
2026-03-09 21:25:16 -0500

    created base home grid

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
 import { useState } from "react";
 import { WalletIcon, ListIcon, HeartIcon, ImageIcon } from "../components/Icons";
 import "../styles/home.css";
+
+export default function HomePage({ wishlist, setWishlist, savings, setSavings }) {
+    return (
+        <div className="page">
+        <div className="home-grid">
+
+          </div>
+        </div>
+      );
+}
