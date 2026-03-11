d439249ca6d0b7029b86ef67e9b147abcfd31454
victor <victorcalehr18@gmail.com>
2026-03-10 23:17:09 -0500

    added imports for wishlist page

---
+++ src/pages/WishlistPage.jsx
@@ -0,0 +1,7 @@
+import { useState, useRef, useEffect } from "react";
+import { HeartIcon, ImageIcon } from "../components/Icons";
+import {
+  BUDGET_OPTIONS, DURATION_OPTIONS, INTEREST_OPTIONS,
+  ALL_DESTINATIONS, PER_PAGE,
+} from "../data/initialData";
+import "../styles/wishlist.css";
